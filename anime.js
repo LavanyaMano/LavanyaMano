c2d4b32c6dc5925aba3fa22761777fea67748b46
Lavanya <lavanya@Lavanyas-MacBook-Pro.local>
2019-05-07 12:58:18 -0400

    template ready

---
+++ anime.js
@@ -0,0 +1,14 @@
+anime({
+  targets: '#polygon',
+  points: [
+  	{ value: '0 0 400 0 800 0 800 500 0 400' },
+    { value: '0 0 400 0 800 300 800 500 0 400' },
+    { value: '0 0 200 0 500 400 800 500 0 400' }
+  ],
+  easing: 'easeOutQuad',
+  duration: 1200,
+  loop: false,
+});
+
+
+console.log("inside anime.js");
